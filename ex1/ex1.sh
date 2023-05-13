8f2d727906f9992890f73669ae7f6f35a54ed6b1
Yaakov Haimoff <hyaakov@aol.com>
2023-05-13 18:36:04 +0300

    finished ex1

---
+++ ex1/ex1.sh
@@ -0,0 +1,12 @@
+touch name.txt
+nano name.txt 
+mkdir new
+cp name.txt new
+mv new/name.txt new/yaakov_haimoff
+open new/yaakov_haimoff
+cp new/yaakov_haimoff new/newFile
+cd cd new
+ls
+cd ..
+cd /Users/yaakovhaimoff
+rm -r /Users/yaakovhaimoff/new
